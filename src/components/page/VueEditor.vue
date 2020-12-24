c4a56969b19d9e09e4edaa2e15b93e7936d51090
YJB <1677953017@qq.com>
2020-12-25 01:45:18 +0800

    08

--- src/components/page/VueEditor.vue
+++ src/components/page/VueEditor.vue
@@ -1,11 +1,31 @@
 <template>
-    <div>
-        <div class="container">
-            <p style='padding-bottom: 10px'>修改密码</p>
-            <el-input style='padding: 10px'>原密码</el-input>
-            <el-input style='padding: 10px'>新密码</el-input>
-            <el-button class="editor-btn" type="primary" @click="submit">提交</el-button>
+    <div class='job-content'>
+<!--        <div class="container">-->
+<!--            <p style='padding-bottom: 10px'>修改密码</p>-->
+<!--            <el-input style='padding: 10px'>原密码</el-input>-->
+<!--            <el-input style='padding: 10px'>新密码</el-input>-->
+<!--            <el-button class="editor-btn" type="primary" @click="submit">提交</el-button>-->
+<!--        </div>-->
+      <el-card class="box-card">
+<!--        <div v-for="o in 4" :key="o" class="text item">-->
+<!--          {{'列表内容 ' + o }}-->
+<!--        </div>-->
+        <el-divider>个人信息</el-divider>
+        <div style='padding-left: 20%'>
+        <div class='text item'><i class='el-icon-paperclip'></i>我的昵称：</div>
+        <div class='text item'><i class='el-icon-paperclip'></i>账号：</div>
+        <div class='text item'><i class='el-icon-paperclip'></i>性别：</div>
+        <div class='text item'><i class='el-icon-paperclip'></i>年龄：</div>
+        <div class='text item'><i class='el-icon-paperclip'></i>是否结婚：</div>
+        <div class='text item'><i class='el-icon-paperclip'></i>地址：</div>
+        <div class='text item'><i class='el-icon-paperclip'></i>电话：</div>
+        <div class='text item'><i class='el-icon-paperclip'></i>邮箱：</div>
+        <div class='text item'><i class='el-icon-paperclip'></i>学校：</div>
+        <div class='text item'><i class='el-icon-paperclip'></i>学历：</div>
+        <div class='text item'><i class='el-icon-paperclip'></i>专业：</div>
+        <div class='text item'><i class='el-icon-paperclip'></i>毕业时间：</div>
         </div>
+      </el-card>
     </div>
 </template>
 
@@ -39,6 +59,16 @@
     }
 </script>
 <style scoped>
+
+.job-content{
+  position:fixed;
+  top:60px;
+  bottom:48px;
+  left:0;
+  font-size:0;
+  width:100%;
+  overflow:auto;
+}
     .editor-btn{
         margin-top: 20px;
     }
@@ -49,5 +79,18 @@
       left:0;
       font-size:20px;
       width:100%;
+    }
+    .text {
+      font-size: 14px;
+    }
+
+    .item {
+      padding: 18px 0;
+    }
+
+    .box-card {
+      width: 100%;
+      position: relative;
+      margin: auto;
     }
 </style>

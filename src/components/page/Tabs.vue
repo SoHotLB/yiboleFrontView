7994ee793f7bf84eab5dcb01389fa635826cbf42
YJB <1677953017@qq.com>
2020-12-26 22:48:00 +0800

    13

--- src/components/page/Tabs.vue
+++ src/components/page/Tabs.vue
@@ -58,8 +58,8 @@
             </div>
             <!-- 分页 -->
             <el-pagination style="padding-top: 15px" @size-change="handleSizeChange" @current-change="handleCurrentChange"
-                           :current-page="currentPage4" :page-sizes="[100, 200, 300, 400]" :page-size="100" layout="total, sizes, prev, pager, next, jumper"
-                           :total="400">
+                           :current-page="currentPage4" :page-sizes="[20]" :page-size="20" layout="total, sizes, prev, pager, next, jumper"
+                           :total="11">
             </el-pagination>
 
     </div>
